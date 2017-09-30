c9c9aef5acc561747304474fb54ec7f3e5d4f994
WytheVan <Wythe110724@hotmail.com>
2017-09-30 11:09:36 +0800

    微信点赞功能1.1(加入分享)

--- activity_page.php
+++ activity_page.php
@@ -1,5 +1,6 @@
 <?php
 	require_once(dirname(__FILE__).'/include/config.inc.php');
+    require_once(dirname(__FILE__).'/include/wechat.class.php');
 
     $aid = $_GET['aid'];
     $pid = $_GET['pid'];
@@ -8,6 +9,15 @@
     if(!empty($msg)){
         echo '<script>alert("'.$msg.'")</script>';
     }
+
+    //取出微信配置
+    $wechat_settings = $dosql->GetOne("SELECT * FROM `#@__wechat_settings` LIMIT 0,1");
+    $app_id = isset($wechat_settings['app_id']) ? $wechat_settings['app_id'] : '';
+    $app_secret = isset($wechat_settings['app_secret']) ? $wechat_settings['app_secret'] : '';
+
+    //获取js参数
+    $wechat_common = new WechatCommon($app_id, $app_secret);
+    $signPackage = $wechat_common->GetSignPackage();
 
     $activity_info = $dosql->GetOne("SELECT * FROM `#@__wechat` WHERE `activity_code` = '$aid' LIMIT 0,1");
     $user_info = $dosql->GetOne("SELECT * FROM `#@__wechat_user` WHERE `uid` = '$pid' LIMIT 0,1");
@@ -69,13 +79,13 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
+    <title><?php echo $activity_info['title'].'-北京中工医药研究院（CIPI）'?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="<?php echo $activity_info['description']?>"/>
     <link rel="stylesheet" href="<?php echo $cfg_webpath; ?>/templates/wechat/css/style.css">
 	<script src="<?php echo $cfg_webpath; ?>/templates/default/js/jquery.js" type="text/javascript"></script>
-	<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript"></script><!--微信JS api-->
-    <title><?php echo $activity_info['title'].'-北京中工医药研究院（CIPI）'?></title>
-	<meta name="description" content="<?php echo $activity_info['description']?>"/>
+	<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
 </head>
 <body>
 <!--头部-->
@@ -228,4 +238,56 @@
 	function do_shear(){
 		window.location.href = 'http://www.cncipi.org/activity.php?aid=<?php echo $aid ?>';
 	}
+
+    //初始化
+        wx.config({
+            debug: false,        //开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: '<?php echo $signPackage["appId"];?>',       //必填，公众号的唯一标识
+            timestamp: <?php echo $signPackage["timestamp"];?>,     //必填，生成签名的时间戳
+            nonceStr: '<?php echo $signPackage["nonceStr"];?>', //必填，生成签名的随机串
+            signature: '<?php echo $signPackage["signature"];?>',      //必填，签名，见附录1
+            jsApiList: [
+                'checkJsApi',
+                'openLocation',
+                'getLocation',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage'
+            ]       //必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+
+        //通过checkJsApi判断当前客户端版本是否支持分享参数自定义
+        wx.checkJsApi({
+            jsApiList: [
+                'getLocation',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage'
+            ],
+            success: function (res) {
+                alert(JSON.stringify(res));
+            }
+        });
+
+        //获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
+        wx.onMenuShareTimeline({
+            title: '<?php echo $activity_info['title'];?>', // 分享标题
+            link: 'http://www.cncipi.org/activity.php?aid=<?php echo $aid ?>&uid=<?php echo $uid ?>&pid=<?php echo $pid ?>', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'http://www.cncipi.org/<?php echo $activity_info['thumb']?>', // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                alert('分享成功！感谢您的支持！');
+            }
+        });
+
+        //获取“分享给朋友”按钮点击状态及自定义分享内容接口
+        wx.onMenuShareAppMessage({
+            title: '<?php echo $activity_info['title'];?>', // 分享标题
+            desc: '<?php echo $activity_info['description'];?>', // 分享描述
+            link: 'http://www.cncipi.org/activity.php?aid=<?php echo $aid ?>&uid=<?php echo $uid ?>&pid=<?php echo $pid ?>', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'http://www.cncipi.org/<?php echo $activity_info['thumb']?>', // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                alert('分享成功！感谢您的支持！');
+            }
+        });
 </script>
